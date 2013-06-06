54f4e73e84eacce84ec4dd06fbc4cc5997f0bced
yury.kaliada <fut.wrk@gmail.com>
2013-06-06 09:44:07 +0300

    Lectures controller
    Signed-off-by: yury.kaliada <fut.wrk@gmail.com>

--- app/controllers/lectures.rb
+++ app/controllers/lectures.rb
@@ -1,1 +1,15 @@
+module LT
+  module Controllers
+    class Lectures < Base
+      # before do
+      #   response.set_cookie :token, TokenGenerator.generate unless cookies[:token]
+      #   @token = cookies[:token]
+      # end
 
+      # get '/l*' do
+      #   binding.pry
+      #   slim :'lectures/show'
+      # end
+    end
+  end
+end
